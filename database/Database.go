3f761d580f9ac83cd7188cff52f3d397e0c62265
Jérémy Giorgio <72692513+JGiorgio-Project@users.noreply.github.com>
2021-10-01 23:24:38 +0200

    delete else

--- database/Database.go
+++ database/Database.go
@@ -14,8 +14,6 @@ func GetDb() (*sql.DB, error) {
 	DbConnection, err := sql.Open("mysql", "db_username:db_password@tcp(localhost:3306)/table_name")
 	if err != nil {
 		panic(err)
-	} else {
-		log.Println("-------------------->Connection DB OK")
 	}
 
 	return DbConnection, err
